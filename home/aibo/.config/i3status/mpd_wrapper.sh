a3ce3bf62a13e5b79ab82e605843879dc603d3a2
Tobias Böhm <aibo@aibor.de>
2013-08-24 00:27:49 +0200

    renamed i3 workspaces and added media key bindings, modified mpd_wrapper_script for i3status

--- home/aibo/.config/i3status/mpd_wrapper.sh
+++ home/aibo/.config/i3status/mpd_wrapper.sh
@@ -2,10 +2,15 @@
 
 i3status | (read line && echo $line && read line && echo $line && while read line
 do 
-  playing="{\"name\":\"mpd\",\"full_text\":\" $(mpc current) \"},"
-  if grep -q 'random: on' <(mpc)
+  playing=''
+  mpc=$(mpc)
+  if grep -q '\[playing\]' <<< "$mpc"
   then
-    line=${line/[/[{\"name\":\"mpd_rnd\",\"color\":\"\#aaaaaa\",\"full_text\":\" RND \"\},}
+    playing="{\"name\":\"mpd\",\"full_text\":\" "$(head -n1 <<< "$mpc")" \"},"
+    if grep -q 'random: on' <<< "$mpc"
+    then
+      line=${line/[/[{\"name\":\"mpd_rnd\",\"color\":\"\#aaaaaa\",\"full_text\":\" RND \"\},}
+    fi
   fi
   echo "${line/[/[$playing}" || exit 1
 done)
